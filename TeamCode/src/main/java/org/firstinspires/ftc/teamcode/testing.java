792eecf3c0f5ad5b42601e6750b678198d1d90b9
Loki7261 <100491484+Loki7261@users.noreply.github.com>
2022-09-19 19:57:41 -0400

    actually adding files
    Roadrunner dependencies added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "testing") //name of opMode on robot
+public class testing extends OpMode {
+
+    //this is logan test method
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
